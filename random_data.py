c0e9c3f84f2ad647d048d00874473d6df16eb74f
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2026-08-01 19:43:03 +0200

    Random Walk implemented

---
+++ random_data.py
@@ -0,0 +1,47 @@
+#Random Data generated through a random walk
+import numpy as np
+import matplotlib.pyplot as plt
+
+def random_walk(start:int, steps:int, bias:float):
+
+    #Input Validation
+    if not isinstance(start, int):
+        raise ValueError("The start point has to be a number!")
+    
+    if not isinstance(steps, int):
+        raise ValueError("The number of steps has to be an integer!")
+    
+    if not isinstance(bias, float) or bias > 1.0 or bias < 0:
+        raise ValueError("The series bais has to be a number between 0 and 1!")
+
+    #Random Walk generation
+    y = start
+
+    time = np.arange(steps + 1)
+    walk = [y]
+    prob = bias
+
+    for i in range(1, steps + 1):
+        if np.random.uniform(0,1) < prob:
+            y += 1
+        else:
+            y -= 1
+        walk.append(y)
+
+    return time, walk
+
+start = int(input("Enter origin point: "))
+steps = int(input("Enter the amount of steps: "))
+bias = float(input("Enter the probability bias: "))
+
+data_random = random_walk(start, steps, bias)
+
+plt.plot(data_random[0], data_random[1], '-', label="Random Walk Data", color = "orange", linewidth = 1)
+
+
+plt.title("Random Data Series")
+plt.xlabel("Steps")
+plt.ylabel("Position")
+plt.legend()
+plt.grid()
+plt.show()
